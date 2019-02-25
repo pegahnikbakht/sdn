adf50a050ceb544484c16f8b88dd520d5b281732
Pegah <pegah.nikbakht_bideh@eit.lth.se>
2019-02-25 10:00:50 +0100

    refactoring: extract method

--- Base functions/network-topo.py
+++ Base functions/network-topo.py
@@ -6,22 +6,24 @@ from mininet.cli import CLI
 
 net = Mininet()
 
-#Add hosts
-h1 = net.addHost('h1')
-h2 = net.addHost('h2')
-h3 = net.addHost('h3')
-h4 = net.addHost('h4')
+def topology():
 
-#Add switches
-s1 = net.addSwitch('s1', protocols='OpenFlow13')
-s2 = net.addSwitch('s2', protocols='OpenFlow13')
+    #Add hosts
+    h1 = net.addHost('h1')
+    h2 = net.addHost('h2')
+    h3 = net.addHost('h3')
+    h4 = net.addHost('h4')
 
-#Add links 
-net.addLink(h1, s1)
-net.addLink(h2, s1)
-net.addLink(h3, s2)
-net.addLink(h4, s2)
-net.addLink(s1, s2)
+    #Add switches
+    s1 = net.addSwitch('s1', protocols='OpenFlow13')
+    s2 = net.addSwitch('s2', protocols='OpenFlow13')
+
+    #Add links 
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s2)
+    net.addLink(h4, s2)
+    net.addLink(s1, s2)
 
 
 def network_start():
@@ -36,4 +38,5 @@ def network_start():
 
 if __name__ == '__main__':
     setLogLevel('info')
+    topology()
     network_start()
